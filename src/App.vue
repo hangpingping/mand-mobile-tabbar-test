0df5327ccfe4da7953a7ab2e858999191adca5e2
hangpingping <half251392@163.com>
2019-09-05 15:04:21 +0800

    提交测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="tab-bar-wrapper">
+      <md-tab-bar class="tab-bar" v-model="current" :items="items" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { TabBar } from "mand-mobile";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    [TabBar.name]: TabBar
+  },
+  data() {
+    return {
+      current: 2,
+      items: [{ name: 1, label: "标签标签" }, { name: 2, label: "标签标签" }]
+    };
   }
 };
 </script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+@import '~minireset.css/minireset.css';
+@import '~mand-mobile/lib-vw/mand-mobile.css';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.tab-bar-wrapper {
+  margin: 4.27vw;
+}
+
+.tab-bar {
+  padding: 0;
+  width: 50%;
+  background-color: #00f0f0;
+}
+
+.tab-bar .md-tab-bar-item {
+  font-size: 3.73333 * 1.4 vw;
+}
 </style>
